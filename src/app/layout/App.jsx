c31acb6ad03a694e4d7ca7dbebcba2da3478406a
Ali Shawky <alish20030@hotmail.com>
2018-09-12 14:49:16 +0200

    Thinking in React

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react';
+
+
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css'
 import './index.css';
-import App from './App';
+import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const rootEl = document.getElementById('root');
+
+let render = () => {
+    ReactDOM.render(<App />, rootEl)
+}
+
+if (module.hot) {
+    module.hot.accept('./app/layout/App', () => {
+        setTimeout(render);
+    })
+}
+
+render();
+
 registerServiceWorker();
