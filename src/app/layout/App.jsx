8674be529c28844ef946a39135bc2e988b457b8c
Ali Shawky <alish20030@hotmail.com>
2018-09-18 20:53:52 +0200

    React Router 4

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react';
+import { Route, Switch } from 'react-router-dom';
 
 
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
 import NavBar from '../../features/nav/NavBar/NavBar';
+import PeopleDashoard from '../../features/user/PeopleDashoard/PeopleDashoard';
+import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
+import EventForm from '../../features/event/EventForm/EventForm';
+import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
+import HomePage from '../../features/home/HomePage';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <Container className="main">
-          <EventDashboard />
-        </Container>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+        </Switch>
+        <Route path="/(.+)" render={() => (
+          <div>
+            <NavBar />
+            <Container className="main">
+              <Switch>
+
+                <Route path='/events' component={EventDashboard} />
+                <Route path='/events/:id' component={EventDetailedPage} />
+                <Route path='/people' component={PeopleDashoard} />
+                <Route path='/profile/:id' component={UserDetailedPage} />
+                <Route path='/settings' component={SettingsDashboard} />
+                <Route path='/createEvent' component={EventForm} />
+              </Switch>
+            </Container>
+          </div>
+        )} />
       </div>
     );
   }
---
+++ src/features/user/PeopleDashoard/PeopleDashoard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PeopleDashoard = () => {
+    return (
+        <div>
+            <h1>People Dashoard</h1>
+        </div>
+    );
+}
+
+export default PeopleDashoard;
