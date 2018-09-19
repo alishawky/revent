ec74b8184e553b9187ec0e7e4e9ccc08187cd753
Ali Shawky <alish20030@hotmail.com>
2018-09-19 22:05:15 +0200

    Introduction to Redux

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -11,6 +11,7 @@ import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
 import EventForm from '../../features/event/EventForm/EventForm';
 import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
 import HomePage from '../../features/home/HomePage';
+import TestComponent from '../../features/testarea/TestComponent';
 
 
 class App extends Component {
@@ -25,8 +26,10 @@ class App extends Component {
             <NavBar />
             <Container className="main">
               <Switch>
-
                 <Route path='/events' component={EventDashboard} />
+
+                <Route path='/test' component={TestComponent} />
+
                 <Route path='/events/:id' component={EventDetailedPage} />
                 <Route path='/people' component={PeopleDashoard} />
                 <Route path='/profile/:id' component={UserDetailedPage} />
